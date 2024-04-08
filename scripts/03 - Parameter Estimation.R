f9fefcd773a348d3861ebe5f835d67d536c0c629
Ben Bodek <bbodek@unc.edu>
2024-04-08 12:39:06 -0400

    Adding code to fit models and extract parameters

--- scripts/03 - Parameter Estimation.R
+++ scripts/03 - Parameter Estimation.R
@@ -6,32 +6,42 @@
 ## parameter estimates to be used in the power simulation
 
 library(lme4)
-library(lmerTest)
+
 # define linear mixed effects model formula to be used
-f<- "cuhdrs ~ 1+ visdy + (1 + visdy| subjid)"
+f<- "cuhdrs ~ 1+ vis.yr + (1 + vis.yr| subjid)"
 
-
-summary(capenriched_df%>%filter(visit==2 & baseline_TFCS>11 & baseline_TMS<20)%>%mutate(delta = baseline_cuhdrs-cuhdrs)%>%select(delta))
-unenriched_df%>%filter(baseline_cuhdrs<5)
-summary(unenriched_df$baseline_TFCS)
-
-
-# select only subjects who have at least 2 study visits
-capenriched_2_df <- capenriched_df %>%
+# select only subjects who have at least 2 study visits and print number of participants
+# for each sub cohort
+capenriched_2viz <- capenriched_df %>%
   group_by(subjid) %>%
-  filter(max(visit) > 1 & baseline_TFCS>11 & baseline_TMS<20 & !is.na(baseline_cuhdrs))
+  filter(max(visit) > 1 )
+nrow(capenriched_2viz%>%select(subjid)%>%unique())
+
+pinenriched_2viz <- pinenriched_df %>%
+  group_by(subjid) %>%
+  filter(max(visit) > 1 )
+nrow(pinenriched_2viz%>%select(subjid)%>%unique())
+
+unenriched_2viz <- unenriched_df %>%
+  group_by(subjid) %>%
+  filter(max(visit) > 1)
+nrow(unenriched_2viz%>%select(subjid)%>%unique())
+
+# create variable vis.yr as visdy/365 to go from days to years
+capenriched_2viz$vis.yr<-capenriched_2viz$visdy/365
+pinenriched_2viz$vis.yr<-pinenriched_2viz$visdy/365
+unenriched_2viz$vis.yr<-unenriched_2viz$visdy/365
+
+# fit linear mixed-effects model for each subcohort
+cap.model<-lmer(formula=f,data = capenriched_2viz)
+pin.model<-lmer(formula=f,data = pinenriched_2viz)
+unenriched.model<-lmer(formula=f,data = unenriched_2viz)
+
+# extract parameter estimates from each model
+# and create unified dataframe for all parameter estimates
+cap.params<-extract.lme.parameters(cap.model,"CAP")
+pin.params<-extract.lme.parameters(pin.model,"PIN")
+unenriched.params<-extract.lme.parameters(unenriched.model,"Unenriched")
+params.df<-rbind(cap.params,pin.params,unenriched.params)
 
 
-test<-selected_df%>%
-  filter(baseline_PIN>0.4)s
-
-#capenriched_2_df$cuhdrs<-capenriched_2_df$cuhdrs/(max(capenriched_2_df$baselins_cuhdrs)-min(capenriched_2_df$baseline_cuhdrs))
-capenriched_2_df$visdy<-capenriched_2_df$visdy/365
-
-model<-lmer(formula = f,data=capenriched_2_df, REML = T,control = lmerControl(optimizer ="bobyqa",optCtrl=list(maxfun=2e5)))
-summary(model)
-library(ggplot2)
-ggplot(unenriched_df, aes(x = visdy, y = cuhdrs, group = subjid, color = as.factor(subjid))) +
-  geom_line() +
-  labs(x = "visdy", y = "cuhdrs") +
-  theme_minimal()
