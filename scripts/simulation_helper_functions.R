9bd2b5a370ee95b673ea08516e38b5faa9f0d3d9
Ben Bodek <bbodek@unc.edu>
2024-04-08 11:53:56 -0400

    Adding function to extract parameters from lme model

--- scripts/simulation_helper_functions.R
+++ scripts/simulation_helper_functions.R
@@ -14,9 +14,30 @@ R<-chol(sigma)
 mvn.sim<-function(n,R,mu){
   X = t(R)%*%matrix(rnorm(n*2),2)+matrix(rep(mu,n),nrow=2,byrow=F)
   X = t(X)
+}
+
+# extract.lme.parameters is a function taking inputs:
+#   model - a linear mixed effectds model with 1 random slope, 1 random intercept,
+#           1 fixed slope, and 1 fixed intercept
+#   name - the desired name of the model to use to reference estimates
+# The function returns a dataframe containing the estimates of the
+# fixed effect parameters, variance of the random effects, residual variance
+# and covariance of the random effects
+extract.lme.parameters<-function(model,name){
+  coef<-fixef(model)
+  fixed.int<-coef[[1]]
+  fixed.slope<-coef[[2]]
+  vc<-as.data.frame(VarCorr(model))
+  var.rndm.int<-vc[1,4]
+  var.rndm.slope<-vc[2,4]
+  covar.rndm<-vc[3,4]
+  var.rndm.int<-vc[4,4]
+  data.frame(model = name,fixed.int = fixed.int, fixed.slope = fixed.slope, 
+             var.rndm.int = var.rndm.int,var.rndm.slope = var.rndm.slope,
+             covar.rndm = covar.rndm)
   }
 
-lme.hyp.test<-function(data,f,)
+
 
 
 
